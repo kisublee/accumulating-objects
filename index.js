23dac1d0d84e38e20ec5d806302123c8941fb7e4
kisublee <yigi2021@gmail.com>
2021-07-22 11:09:31 -0400

    Add solutions.

--- index.js
+++ index.js
@@ -37,7 +37,21 @@ const studentsArr = [
  * @returns {String} A string containing the names of all the students from the `students` array.
  */
 function stringNames(students) {
+  // returns the names of each student in the class
+  // Input: an array of objects
+  // Output: A string with the names of all the students
 
+  // Define a default value
+  let studentNames = " ";
+  // Loop
+  for (i = 0; i < students.length; i++) {
+    //Accumulating 
+      let studentName = students[i]
+    if (studentName.name) {// If a student in the array has a name
+      studentNames += studentName.name + ", " 
+    }
+  }
+    return studentNames
 }
 
 stringNames(studentsArr);
@@ -49,7 +63,14 @@ stringNames(studentsArr);
  * @returns {String[]} An array of the names of all the students from the `students` array.
  */
 function arrayNames(students) {
-
+  let studentNames = []; // add names to this array
+  for (i = 0; i < students.length; i++) {
+    let studentName = students[i]
+    if (studentName.name) {
+      studentNames.push(studentName.name)
+    }
+  }  console.log(studentNames)
+    return studentNames
 }
 
 arrayNames(studentsArr);
@@ -62,7 +83,21 @@ arrayNames(studentsArr);
  * @returns {Object} The student in the class whose name matches.
  */
 function findByName(students, name) {
+ 
+    // Search for a student by NAME
+    // Input: An array of objects
+    // Input2: The name of the student to find
+    // Output: return an object with the name of the student that matches with 'name' parameter
 
+    let studentName = {}; // define  a default value
+
+    for (const name of students) {
+        // Accumulating
+      if (students.name === name) {
+        studentName = students.name
+      }
+    } console.log(studentName)
+    return studentName
 }
 
 findByName(studentsArr, "Clementine Bauch");
